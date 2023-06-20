ac3a0ac64e0b3f01d90c0b9cf18290f4e13fbc33
francocirioni <franco.cirioni.cssm@gmail.com>
2023-06-20 14:31:37 -0300

    su

--- vue.js
+++ vue.js
@@ -8,8 +8,9 @@ new Vue({
   },
 
   methods: {
+
     obtenerProductos() {
-      axios.get('https://misutilidades-a9f69-default-rtdb.firebaseio.com/productos.json')
+      axios.get('http://francocir.pythonanywhere.com/productos')
         .then(response => {
           this.productos = Object.values(response.data);
         })
@@ -17,50 +18,19 @@ new Vue({
           console.error(error);
         });
     },
-    eliminarProducto(nombre) {
-      const index = this.productos.findIndex(producto => producto.nombre === nombre);
-      if (index !== -1) {
-        this.productos.splice(index, 1); // Eliminar el producto de la lista local
 
-        axios.put('https://misutilidades-a9f69-default-rtdb.firebaseio.com/productos.json', this.productos)
+    eliminarProducto(nombre) {
+
+      const index = this.productos.findIndex(producto => producto.nombre === nombre);
+
+      if (index !== -1) {
+        this.productos.splice(index, 1);
+    }
+
+        axios.put('http://francocir.pythonanywhere.com/productos', this.productos)
           .then(response => {
             console.log('Producto eliminado de la base de datos');
           })
-          .catch(error => {
-            console.error(error);
-          });
-      }
-    },
+  }}
 
-
-    //         crearProducto() {
-    //      const nombre = document.getElementById('nombre').value;
-    //    const version = document.getElementById('version').value;
-    //  const descripcion = document.getElementById('descripcion').value;
-    //const icono = document.getElementById('icono').value;
-    //const url = document.getElementById('url').value;
-
-    //if (nombre && version && descripcion && icono && url) {
-    //const nuevoProducto = {
-    //nombre: nombre,
-    //version: version,
-    //descripcion: descripcion,
-    //icono: icono,
-    //url: url
-    //};
-
-    //this.productos.push(nuevoProducto); // Agregar el nuevo producto a la lista local
-
-    //axios.put('https://misutilidades-a9f69-default-rtdb.firebaseio.com/productos.json', this.productos)
-    //.then(response => {
-    // console.log('Nuevo producto creado y guardado en la base de datos');
-    //})
-    //.catch(error => {
-    // console.error(error);
-    //});
-    //} else {
-    //console.log('Por favor, complete todos los campos');
-    //}
-    //}
-  }
 });
