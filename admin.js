b0b85ea56f17a1041b911c6c8d8ef258ea94fde7
francocirioni <101602566+francocirioni@users.noreply.github.com>
2023-06-29 09:55:53 -0300

    hggh

--- admin.js
+++ admin.js
@@ -1,3 +1,18 @@
 function redireccionar() {
     window.location.href = 'index.html';
   }
+
+
+  const inputContraseña = document.getElementById("inputContraseña");
+  const botones = document.getElementsByClassName("contenedorbotones");
+  
+  document.getElementById("btnIngresar").addEventListener("click", function() {
+    const contraseña = inputContraseña.value;
+  
+    if (contraseña === "admin") {
+
+        window.location.href = "index2.html";
+      
+    }else { window.alert("contraseña incorrecta");}
+   
+  });
